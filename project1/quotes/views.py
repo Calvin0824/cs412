be53f133eec9eadb7102b47a06af4326ca1b6d2a
“Calvin <Calvinli0824@gmail.com>
2024-09-23 00:33:22 -0400

    all finished except css

--- project1/quotes/views.py
+++ project1/quotes/views.py
@@ -13,23 +13,23 @@ QUOTES_PICS = list(zip(QUOTES, PICS))
 
 def home(request):
     """the home page where it will display a random image and a random quote"""
-    quote, pic = random.choice(QUOTES_PICS)
-    context = {"quote": quote, "pic": pic}
-    return render(request, "templates/home.html", context)
+    quote_pic = random.choice(QUOTES_PICS)
+    context = {"quote": quote_pic[0], "pic": quote_pic[1]}
+    return render(request, "quotes/quote.html", context)
 
 def quote(request):
     """the quote page where it will display a random quote"""
     quote = random.choice(QUOTES)
     pic = random.choice(PICS)
     context = {"quote": quote, "pic": pic}
-    return render(request, "quote.html", context)
+    return render(request, "quotes/quote.html", context)
 
 def show_all(request):
     """this page will display all quotes and images"""
     quotes = QUOTES
     pics = PICS
-    return render(request, "show_all.html", {"quotes": quotes, "pics": pics})
+    return render(request, "quotes/show_all.html", {"quotes": quotes, "pics": pics})
 
 def about(request):
     """this page will display information about the author"""
-    return render(request, "about.html")
+    return render(request, "quotes/about.html")
