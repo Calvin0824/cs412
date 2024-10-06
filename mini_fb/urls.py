5426f6f57abb1138a64fb31f4691dea6f587a8a9
“Calvin <Calvinli0824@gmail.com>
2024-10-06 00:28:48 -0400

    modified urls.py

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ShowAllProfileViews.as_view(), name='show_all_profiles'),
+    path(r'', views.ShowAllProfileViews.as_view(), name='show_all_profiles'),
 ]
