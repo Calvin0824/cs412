b2337ceaedeeae866ea6022a53622060014dc282
“Calvin <Calvinli0824@gmail.com>
2024-10-05 22:53:17 -0400

    Created Profile model for mini_fb

---
+++ mini_fb/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Profile(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    city = models.TextField(blank=False)
+    email = models.EmailField()
+    image = models.ImageField(blank=True)
