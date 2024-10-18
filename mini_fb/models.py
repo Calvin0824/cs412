c7d2a9269309768b078fbef9b951f88188a3483f
“Calvin <Calvinli0824@gmail.com>
2024-10-18 17:24:52 -0400

    images added to status message

--- mini_fb/models.py
+++ mini_fb/models.py
@@ -10,11 +10,12 @@ class Profile(models.Model):
     image = models.URLField(blank=True)
 
     def get_status_messages(self):
-        """Returns the status messages for this profile"""
+        '''Returns the status messages for this profile'''
         messages = StatusMessage.objects.filter(profile=self.profile)
         return messages
     
     def get_absolute_url(self):
+        '''Return the URL to display this profile'''
         return reverse('show_profile', kwargs={'pk': self.pk})
 
 class StatusMessage(models.Model):
@@ -25,3 +26,13 @@ class StatusMessage(models.Model):
     def __str__(self):
         '''Return the string representation of this status message.'''
         return f'{self.message}'
+    
+    def get_images(self):
+        '''Returns the images for this status message'''
+        images = Image.objects.filter(message=self)
+        return images
+    
+class Image(models.Model):
+    img = models.ImageField()
+    message = models.ForeignKey(StatusMessage, on_delete=models.CASCADE, related_name='images')
+    uploaded = models.DateTimeField(auto_now_add=True)
